5bbf28f91ead1bdce0c0abfcac9da341b6b96b10
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-04-29 21:20:21 +0900

    chore: 폰트 저장 위치 변경
    - /public -> /src/assets/fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
+import { pretendard } from '@/assets/fonts';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
-
-const pretendard = localFont({
-  src: '../../public/fonts/pretendard/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '100 900',
-  variable: '--font-pretendard',
-});
 
 export const metadata: Metadata = {
   title: 'Pumati',
