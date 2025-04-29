22f4a5771cae96ac026378a79bfc5e5322de5a8c
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-04-29 20:50:05 +0900

    feat: TextInput 컴포넌트 구현
    - 텍스트 입력 컴포넌트 구현
    
    #4

---
+++ src/components/text-input/TextInput.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+
+export interface TextInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+  required?: boolean;
+}
+
+export default function TextInput({
+  name,
+  label,
+  required,
+  ...rest
+}: TextInputProps) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const errorMessage = errors[name]?.message;
+
+  return (
+    <article className="relative flex flex-col gap-2 px-5 pt-3 pb-7 max-w-[540px] w-full">
+      <label className="font-medium">
+        {label} {required && <span className="text-red-500">*</span>}
+      </label>
+      <input
+        {...register(name)}
+        className={`px-4 py-3 placeholder:text-grey rounded-md outline-none border focus:border-transparent border-grey focus:ring-2 focus:ring-blue focus:ring-offset-0`}
+        {...rest}
+      />
+      {errorMessage && (
+        <p className="absolute bottom-0 text-sm text-red-500">
+          {String(errorMessage)}
+        </p>
+      )}
+    </article>
+  );
+}
