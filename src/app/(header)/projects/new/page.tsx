a103ce1d674a5f383f074880f0c6be2f2cf675a8
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-05 15:59:16 +0900

    feat: 프로젝트 생성 페이지 생성

---
+++ src/app/(header)/projects/new/page.tsx
@@ -0,0 +1,10 @@
+import { CreateForm } from '@/features/project/components/new/CreateForm';
+
+export default function NewProjectPage() {
+  return (
+    <section className="flex flex-col items-center min-h-[calc(100vh-6rem)]">
+      <h1 className="text-xl font-semibold mt-9">프로젝트 생성</h1>
+      <CreateForm />
+    </section>
+  );
+}
