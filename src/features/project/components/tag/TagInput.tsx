37740aa78d22c1192584caca1e670077cc36a97d
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-05 19:41:58 +0900

    feat: 최대 태그 개수 알림 텍스트 추가

--- src/features/project/components/tag/TagInput.tsx
+++ src/features/project/components/tag/TagInput.tsx
@@ -19,13 +19,16 @@ export function TagInput({ value, onChange, maxTags = 5 }: TagInputProps) {
 
   return (
     <div className={cn('flex flex-col w-full', value.length !== 0 && 'gap-2')}>
+      <p className="text-grey text-sm absolute top-9 left-0">
+        * 태그는 최대 5개까지 입력할 수 있습니다.
+      </p>
       <input
         type="text"
         value={tagValue}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
         placeholder="태그를 입력해주세요 (Enter로 추가)"
-        className="px-4 py-3 placeholder:text-grey rounded-md outline-none border focus:border-transparent border-grey focus:ring-2 focus:ring-blue focus:ring-offset-0 disabled:bg-blue-white"
+        className="px-4 py-3 mt-4 placeholder:text-grey rounded-md outline-none border focus:border-transparent border-grey focus:ring-2 focus:ring-blue focus:ring-offset-0 disabled:bg-blue-white"
         disabled={value.length >= maxTags}
       />
       <TagList tags={value} removeTag={removeTag} />
