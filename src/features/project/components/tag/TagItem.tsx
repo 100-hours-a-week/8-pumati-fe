c8399d6868e83f5a01b62fb30f36f285a8a03643
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-06 15:03:47 +0900

    feat: 태그 컴포넌트 확장
    - 삭제 가능 여부에 따른 디자인 변경

--- src/features/project/components/tag/TagItem.tsx
+++ src/features/project/components/tag/TagItem.tsx
@@ -2,23 +2,33 @@ import { CancelIcon } from '@/components/icons';
 
 type TagItemProps = {
   tag: string;
-  removeTag: () => void;
+  removeTag?: () => void;
 };
 
 export function TagItem({ tag, removeTag }: TagItemProps) {
+  const isRemoveTag = !!removeTag;
+
+  const handleRemoveTag = () => {
+    if (isRemoveTag) {
+      removeTag();
+    }
+  };
+
   return (
     <li
-      className="flex items-center gap-[6px] px-3 py-1 bg-light-blue rounded-full cursor-pointer transition-all duration-100 hover:brightness-95"
-      onClick={removeTag}
+      className="flex items-center gap-[6px] px-3 py-1 bg-light-blue border border-soft-blue rounded-full cursor-pointer transition-all duration-100 hover:brightness-95"
+      onClick={handleRemoveTag}
     >
       <span className="text-blue text-sm font-medium">{tag}</span>
-      <CancelIcon
-        width={8}
-        height={8}
-        fill="var(--color-blue)"
-        stroke="var(--color-blue)"
-        strokeWidth="2"
-      />
+      {isRemoveTag && (
+        <CancelIcon
+          width={8}
+          height={8}
+          fill="var(--color-blue)"
+          stroke="var(--color-blue)"
+          strokeWidth="2"
+        />
+      )}
     </li>
   );
 }
