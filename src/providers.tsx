e3e940d95284330a606711f7354bcf9ed2d69004
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-06 19:02:23 +0900

    feat: tanstack query 초기 설정 및 devtools 설치

---
+++ src/providers.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { getQueryClient } from '@/libs/tanstack-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={getQueryClient()}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
