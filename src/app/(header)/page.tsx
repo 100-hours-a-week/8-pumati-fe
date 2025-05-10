cc255ee602e15c47e851667e4321d0a5348460d4
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-10 17:08:51 +0900

    design: 품앗이 상위 랭킹 프로젝트 목록 및 메인페이지에 적용

--- src/app/(header)/page.tsx
+++ src/app/(header)/page.tsx
@@ -1,3 +1,77 @@
+import { ProjectGallery } from '@/features/project/components';
+import { SimpleCardList } from '@/features/project/components/simple-card-list';
+import { ProjectItem } from '@/features/project/schemas';
+import { Attendance } from '@/features/user/components/attendance';
+
+const latestProjects: ProjectItem[] = [
+  {
+    id: 1,
+    teamId: 101,
+    term: 2,
+    teamNumber: 1,
+    badgeImageUrl:
+      'https://s3-pumati-test.s3.ap-northeast-2.amazonaws.com/uploads/69f83a96-2e07-457a-84ba-27de234961f6.jpg',
+    representativeImageUrl:
+      'https://s3-pumati-test.s3.ap-northeast-2.amazonaws.com/uploads/69f83a96-2e07-457a-84ba-27de234961f6.jpg',
+    title: 'pumati',
+    introduction:
+      '카카오 테크 부트캠프 교육생의 성장 과정을 한눈에 보고, 출석까지 관리할 수 있는 교육생 중심 플랫폼입니다.카카오 테크 부트캠프 교육생의 성장 과정을 한눈에 보고, 출석까지 관리할 수 있는 교육생 중심 플랫폼입니다.',
+    tags: [
+      '풀스택',
+      '품앗이awegaawe',
+      '품앗이awegaawe',
+      '품앗이awegaawe',
+      '품앗이awegaawe',
+    ],
+    givedPumatiCount: 120,
+    receivedPumatiCount: 95,
+    createdAt: '2025-05-01T10:15:30Z',
+    modifiedAt: '2025-05-05T14:22:10Z',
+  },
+  {
+    id: 2,
+    teamId: 102,
+    term: 2,
+    teamNumber: 2,
+    badgeImageUrl:
+      'https://s3-pumati-test.s3.ap-northeast-2.amazonaws.com/uploads/69f83a96-2e07-457a-84ba-27de234961f6.jpg',
+    representativeImageUrl:
+      'https://s3-pumati-test.s3.ap-northeast-2.amazonaws.com/uploads/69f83a96-2e07-457a-84ba-27de234961f6.jpg',
+    title: 'goorm Dive',
+    introduction:
+      '실무자와 함께 하는 실전 프로젝트 경험, 맞춤형 멘토링을 제공하는 Deep Dive 프로그램입니다.',
+    tags: ['AI', '클라우드', '품앗이클라우드클라우드클라우', '품앗이awegaawe'],
+    givedPumatiCount: 75,
+    receivedPumatiCount: 80,
+    createdAt: '2025-04-28T09:00:00Z',
+    modifiedAt: '2025-05-03T16:45:00Z',
+  },
+  {
+    id: 3,
+    teamId: 103,
+    term: 3,
+    teamNumber: 1,
+    badgeImageUrl:
+      'https://s3-pumati-test.s3.ap-northeast-2.amazonaws.com/uploads/69f83a96-2e07-457a-84ba-27de234961f6.jpg',
+    representativeImageUrl:
+      'https://s3-pumati-test.s3.ap-northeast-2.amazonaws.com/uploads/69f83a96-2e07-457a-84ba-27de234961f6.jpg',
+    title: 'Campus Connect',
+    introduction:
+      '교육생들 간 네트워킹과 협업을 돕는 플랫폼으로, 프로젝트 콜라보레이션을 쉽게 관리할 수 있습니다.',
+    tags: ['풀스택', '협업'],
+    givedPumatiCount: 45,
+    receivedPumatiCount: 50,
+    createdAt: '2025-05-02T11:20:00Z',
+    modifiedAt: '2025-05-06T08:30:00Z',
+  },
+];
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <section className="flex flex-col gap-4 pb-15">
+      <SimpleCardList projects={latestProjects} />
+      <Attendance />
+      <ProjectGallery projects={latestProjects} />
+    </section>
+  );
 }
---
+++ src/features/project/components/simple-card-item/SimpleCardItem.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { PROJECT_PATH } from '@/constants';
+import { cn } from '@/utils/style';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { ProjectItem } from '../../schemas';
+import { TagList } from '../tag';
+
+type SimpleCardItemProps = {
+  project: ProjectItem;
+  rank?: number;
+};
+
+export function SimpleCardItem({ project, rank }: SimpleCardItemProps) {
+  const { id, representativeImageUrl, title, introduction, teamNumber, tags } =
+    project;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(PROJECT_PATH.DETAIL(id.toString()));
+  };
+  return (
+    <li
+      className="flex flex-col gap-2 cursor-pointer px-4 py-3 border rounded-lg border-blue"
+      onClick={handleClick}
+    >
+      <div className="flex gap-4">
+        <div className="relative w-18 xs:w-22 h-18 xs:h-22 shrink-0">
+          <Image
+            src={representativeImageUrl}
+            alt={title}
+            fill
+            sizes="100%"
+            className="object-cover rounded-lg"
+          />
+          {rank && (
+            <span
+              className={cn(
+                'absolute overflow-hidden -top-1 -left-1 rounded-full w-6 h-6 z-10 border-2 flex items-center justify-center text-white text-sm font-bold',
+                rank === 1
+                  ? 'bg-[#FFD700] border-[#E6C200]'
+                  : rank === 2
+                    ? 'bg-[#C0C0C0] border-[#A8A8A8]'
+                    : 'bg-[#CD7F32] border-[#B87333]',
+              )}
+            >
+              <span className="absolute -bottom-1 -left-2 w-10 h-2 bg-white opacity-30 transform -rotate-45 pointer-events-none" />
+              {rank}
+            </span>
+          )}
+        </div>
+        <div className="">
+          <div className="flex justify-between items-center">
+            <h3 className="font-bold text-lg truncate">{title}</h3>
+            <span className="text-sm text-dark-grey font-semibold">
+              {teamNumber}팀
+            </span>
+          </div>
+          <p className="mt-1 text-sm xs:text-[15px] text-dark-grey line-clamp-2">
+            {introduction}
+          </p>
+        </div>
+      </div>
+      <TagList tags={tags.slice(0, 3)} />
+    </li>
+  );
+}
