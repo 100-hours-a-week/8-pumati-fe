64df46cb88f3aff74532bd53f1e7c3b1d889a874
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-11 02:59:48 +0900

    feat: 프로젝트 목록 무한 스크롤 적용
    - observer를 등록하고, 관찰되면 함수를 실행하는 useIntersectionObserver 커스텀 훅 구현

--- src/features/project/components/projects-container/ProjectsContainer.tsx
+++ src/features/project/components/projects-container/ProjectsContainer.tsx
@@ -2,6 +2,7 @@
 
 import { CallToAction } from '@/components';
 import { PROJECT_PATH } from '@/constants';
+import { useIntersectionObserve } from '@/hooks';
 import { useRouter } from 'next/navigation';
 import { useProjects } from '../../hooks';
 import { CardList } from '../card-list';
@@ -13,17 +14,30 @@ type ProjectsContainerProps = {
 export function ProjectsContainer({ contextId }: ProjectsContainerProps) {
   const router = useRouter();
 
-  const { data, fetchNextPage, hasNextPage } = useProjects(contextId);
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useProjects(contextId);
   const projects = data.pages.flatMap((page) => page.data);
+
+  const ref = useIntersectionObserve({
+    onIntersect: (entry, observer) => {
+      observer.unobserve(entry.target);
+
+      if (hasNextPage && !isFetchingNextPage) fetchNextPage();
+    },
+  });
   return (
-    <div className="flex flex-col items-center gap-4 w-full max-w-[25rem] mb-25">
+    <div className="flex flex-col items-center gap-4 w-full max-w-[25rem]">
       <h1 className="text-xl font-semibold my-9">프로젝트 둘러보기</h1>
       <CallToAction
         text="프로젝트를 생성해보세요!"
         buttonText="생성하기"
         action={() => router.push(PROJECT_PATH.NEW)}
       />
-      <CardList projects={projects} />
+      <CardList
+        ref={ref}
+        isFetchingNextPage={isFetchingNextPage}
+        projects={projects}
+      />
     </div>
   );
 }
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export * from './useIntersectionObserver';
 export * from './useMultiFilesToS3';
 export * from './useOutsideClick';
 export * from './useUploadFileToS3';
