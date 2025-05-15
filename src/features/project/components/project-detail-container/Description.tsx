21185e9cdec73269187a4f22a77dc309fa3ee2e5
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-15 19:02:25 +0900

    fix: 프로젝트 한 줄 소개 줄바꿈

--- src/features/project/components/project-detail-container/Description.tsx
+++ src/features/project/components/project-detail-container/Description.tsx
@@ -73,7 +73,9 @@ export function Description({
           </div>
         </div>
       </div>
-      <p className="leading-5 mb-4">{introduction}</p>
+      <p className="leading-5 mb-4 whitespace-pre-wrap break-words">
+        {introduction}
+      </p>
       <Button onClick={handleOpenProject}>프로젝트 바로가기</Button>
       <TagList tags={tags.map((tag) => tag.content)} />
       <div className="flex flex-col gap-4 mt-16 mb-10">
