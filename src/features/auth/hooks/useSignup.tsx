2f0006ac2dafd1641af8aa6e9235b5e3749cc74e
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-05-16 16:14:13 +0900

    fix: 회원가입/프로젝트 생성 실패 시 alert 피드백

--- src/features/auth/hooks/useSignup.tsx
+++ src/features/auth/hooks/useSignup.tsx
@@ -1,9 +1,13 @@
+import { AUTH_PATH } from '@/constants';
 import { accessTokenAtom } from '@/store';
 import { useMutation } from '@tanstack/react-query';
 import { useSetAtom } from 'jotai';
+import { useRouter } from 'next/navigation';
 import { signup } from '../services';
 
 export function useSignup() {
+  const router = useRouter();
+
   const setAccessToken = useSetAtom(accessTokenAtom);
 
   return useMutation({
@@ -12,7 +16,8 @@ export function useSignup() {
       setAccessToken(data.accessToken);
     },
     onError: (error) => {
-      // 회원가입 실패 처리 Toast
+      alert('회원가입에 실패했습니다. 잠시후 다시 시도해주세요.');
+      router.replace(AUTH_PATH.LOGIN);
       console.error(error);
     },
   });
