777c9c759bc6b2299781b11e081499a81ff7ab27
yeol2 <qkrdufdl358@naver.com>
2025-06-10 16:45:34 +0900

    feat: 클라우드 테스트-17

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
+// next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        // hostname: process.env.NEXT_PUBLIC_S3_HOSTNAME!,
-        hostname: 's3-pumati-prod.s3.ap-northeast-2.amazonaws.com',
-      },
-    ],
+    domains: ['s3-pumati-prod.s3.ap-northeast-2.amazonaws.com'],
   },
 };
 
