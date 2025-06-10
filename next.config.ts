e3f9ddeea90c20ccf2e36d7bb8bbaa344c276c4b
yeol2 <qkrdufdl358@naver.com>
2025-06-10 15:52:48 +0900

    fix: s3 image url

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         // hostname: process.env.NEXT_PUBLIC_S3_HOSTNAME!,
-        hostname: 's3-pumati-prod.s3.ap-northeast-2.amazonaws.com'
+        hostname: 's3-pumati-prod.s3.ap-northeast-2.amazonaws.com',
       },
     ],
   },
