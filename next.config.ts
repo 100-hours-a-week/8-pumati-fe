e5cde49bac9e9b67d732b788aee7dcda4b6c3c42
yeol2 <qkrdufdl358@naver.com>
2025-06-11 11:34:03 +0900

    feat: 클라우드 테스트-26

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_S3_HOSTNAME!,
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
