bacf0cfa0ec488e347e7674fdd74f5ead56ae94d
yeol2 <qkrdufdl358@naver.com>
2025-06-11 11:15:11 +0900

    feat: 클라우드 테스트-23

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   images: {
     remotePatterns: [
-      new URL(`https://${process.env.NEXT_PUBLIC_S3_HOSTNAME!}/**`),
+      new URL(`https://${process.env.NEXT_PUBLIC_S3_HOSTNAME}/**`),
     ],
   },
 };
