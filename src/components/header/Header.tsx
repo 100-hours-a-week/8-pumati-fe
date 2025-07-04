a5ab5680621fdc54a3cf914ff8b7d92f29ba977f
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-07-04 19:21:22 +0900

    style: 뒤로가기 버튼 생성
    - 메뉴에 있는 페이지를 제외한 페이지에 뒤로가기 버튼 생성

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,14 +10,11 @@ type HeaderProps = {
 export function Header({ title }: HeaderProps) {
   const router = useRouter();
 
-  const handleBack = () => {
-    router.back();
-  };
   return (
     <header className="sticky top-0 flex justify-center items-center px-6 py-4 w-full h-16 border-b border-soft-grey bg-white z-40">
       <button
-        onClick={handleBack}
         className="absolute left-6 cursor-pointer"
+        onClick={() => router.back()}
         aria-label="뒤로가기"
       >
         <NavArrowIcon
--- src/components/menu-header/MenuHeader.tsx
+++ src/components/menu-header/MenuHeader.tsx
@@ -1,17 +1,24 @@
 'use client';
 
+import { PROJECT_PATH, ROOT_PATH, USER_PATH } from '@/constants';
 import { navbarAtom } from '@/store/atoms';
 import { cn } from '@/utils/style';
 import { useAtom } from 'jotai';
 import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 import { useRef } from 'react';
-import { CancelIcon, LogoIcon, MenuIcon } from '../icons';
+import { CancelIcon, LogoIcon, MenuIcon, NavArrowIcon } from '../icons';
 import { NavBar } from './NavBar';
 
-export function MenuHeader() {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const [isNavbarOpen, setIsNavbarOpen] = useAtom(navbarAtom);
+const NO_BACK_BUTTON_PATHS = [ROOT_PATH, PROJECT_PATH.ROOT, USER_PATH.MY_PAGE];
 
+export function MenuHeader() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const [isNavbarOpen, setIsNavbarOpen] = useAtom(navbarAtom);
   return (
     <header className="sticky top-0 w-full bg-white overflow-hidden z-40">
       <section
@@ -22,13 +29,29 @@ export function MenuHeader() {
             : 'transition-colors duration-300 ease-in-out',
         )}
       >
+        <div className="w-6 h-6">
+          {!NO_BACK_BUTTON_PATHS.includes(pathname) && (
+            <button
+              aria-label="뒤로가기"
+              onClick={() => router.back()}
+              className="cursor-pointer"
+            >
+              <NavArrowIcon
+                width={24}
+                height={24}
+                fill="var(--color-dark-grey)"
+                className="-rotate-90"
+              />
+            </button>
+          )}
+        </div>
         <Link href="/" aria-label="홈으로 이동">
           <LogoIcon width={92} />
         </Link>
         <button
           ref={buttonRef}
           type="button"
-          className="cursor-pointer"
+          className="cursor-pointer w-6 h-6"
           aria-label={isNavbarOpen ? '메뉴 닫기' : '메뉴 열기'}
           aria-expanded={isNavbarOpen}
           aria-controls="navbar-menu"
