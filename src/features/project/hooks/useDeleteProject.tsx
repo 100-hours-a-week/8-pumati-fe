0fd530f5197c82a28ffcaa550b913ea1bf709d24
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-07-09 17:01:37 +0900

    fix: 프로젝트 생성 및 삭제 시 마이페이지 대시보드 쿼리 무효화
    - 프로젝트가 삭제되었는데 그대로 대시보드가 남아있는 문제 해결을 위해

--- src/features/project/hooks/useDeleteProject.tsx
+++ src/features/project/hooks/useDeleteProject.tsx
@@ -1,6 +1,6 @@
-import { PROJECT_PATH, PROJECT_QUERY_KEY } from '@/constants';
+import { PROJECT_PATH, PROJECT_QUERY_KEY, USER_QUERY_KEY } from '@/constants';
 import { getQueryClient } from '@/libs/tanstack-query';
-import { accessTokenAtom } from '@/store/atoms';
+import { accessTokenAtom, authAtom } from '@/store/atoms';
 import { useMutation } from '@tanstack/react-query';
 import { useAtomValue } from 'jotai';
 import { useRouter } from 'next/navigation';
@@ -9,6 +9,7 @@ import { deleteProject } from '../services';
 export function useDeleteProject() {
   const router = useRouter();
 
+  const authData = useAtomValue(authAtom);
   const accessToken = useAtomValue(accessTokenAtom);
 
   const queryClient = getQueryClient();
@@ -19,6 +20,9 @@ export function useDeleteProject() {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: PROJECT_QUERY_KEY.SUBSCRIPTION_DATA,
+      });
+      queryClient.invalidateQueries({
+        queryKey: USER_QUERY_KEY.DASHBOARD(authData!.teamId!),
       });
       router.push(PROJECT_PATH.ROOT);
     },
--- src/features/user/components/dashboard/DashboardItem.tsx
+++ src/features/user/components/dashboard/DashboardItem.tsx
@@ -2,7 +2,7 @@ import { abbreviateCountToK } from '@/utils/count';
 import { cn } from '@/utils/style';
 
 type DashboardItemProps = {
-  value: number;
+  value: number | null;
   unit: string;
   textColor: string;
 };
@@ -10,7 +10,7 @@ type DashboardItemProps = {
 export function DashboardItem({ value, unit, textColor }: DashboardItemProps) {
   return (
     <p className={cn('font-bold text-base xs:text-xl', textColor)}>
-      {abbreviateCountToK(value)}
+      {value !== null ? abbreviateCountToK(value) : '-'}
       <span className="text-xs xs:text-sm">{unit}</span>
     </p>
   );
