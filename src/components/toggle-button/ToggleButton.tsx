fc3e3fe9fd2dbf477fcf4b0d9688849125bae059
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-07-10 11:10:38 +0900

    feat: 토글 버튼 구현 및 적용

---
+++ src/components/toggle-button/ToggleButton.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { cn } from '@/utils/style';
+import { useState } from 'react';
+
+type ToggleButtonProps = {
+  initialValue: boolean;
+  onToggle: (isOn: boolean) => void;
+};
+
+export function ToggleButton({ initialValue, onToggle }: ToggleButtonProps) {
+  const [isOn, setIsOn] = useState(initialValue);
+
+  const handleToggle = () => {
+    setIsOn((prev) => !prev);
+    onToggle(isOn);
+  };
+  return (
+    <button
+      type="button"
+      aria-pressed={isOn}
+      onClick={handleToggle}
+      className={cn(
+        'w-12 h-7 rounded-full border-none outline-none transition-colors duration-200 relative p-0 inline-block shadow-sm cursor-pointer',
+        isOn ? 'bg-blue' : 'bg-light-grey',
+      )}
+    >
+      <span
+        className={cn(
+          'absolute top-1 left-1 w-5 h-5 rounded-full bg-white shadow-lg transition-all duration-200',
+          isOn && 'translate-x-5',
+        )}
+      />
+    </button>
+  );
+}
--- src/features/user/components/dashboard/DashboardContainer.tsx
+++ src/features/user/components/dashboard/DashboardContainer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { ErrorHandlingWrapper } from '@/components';
+import { ErrorHandlingWrapper, ToggleButton, Tooltip } from '@/components';
+import { NavArrowIcon } from '@/components/icons';
 import { authAtom } from '@/store/atoms';
 import { useAtomValue } from 'jotai';
 import { DashboardErrorFallback } from './DashboardErrorFallback';
@@ -20,6 +21,23 @@ export function DashboardContainer() {
       >
         <DashboardFetcher />
       </ErrorHandlingWrapper>
+      <div className="flex justify-between items-center w-fullw-full mt-4">
+        <div className="flex items-center gap-3">
+          <Tooltip direction="right">
+            <p>
+              이메일을 수신에 동의하면 매주 월요일 주간 품앗이 리포트가
+              발송됩니다.
+            </p>
+          </Tooltip>
+          <p className="font-semibold text-dark-grey">
+            주간 품앗이 리포트 이메일 수신여부
+          </p>
+          <button className="cursor-pointer">
+            <NavArrowIcon width={20} height={20} className="rotate-90" />
+          </button>
+        </div>
+        <ToggleButton initialValue={false} onToggle={() => {}} />
+      </div>
     </div>
   );
 }
