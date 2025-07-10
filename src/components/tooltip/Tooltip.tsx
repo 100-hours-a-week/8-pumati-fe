9c2685fde24428f12c316cee904ee75e6d78b3ec
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-07-10 11:07:31 +0900

    refactor: Tooltip 컴포넌트 공통 컴포넌트로 수정

---
+++ src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,28 @@
+import { InfoIcon } from '@/components/icons';
+import { cn } from '@/utils/style';
+
+type TooltipProps = {
+  children: React.ReactNode;
+  direction?: 'right' | 'left';
+};
+
+export function Tooltip({ children, direction = 'left' }: TooltipProps) {
+  return (
+    <div className="relative group">
+      <InfoIcon
+        width={22}
+        height={22}
+        stroke="var(--color-dark-grey)"
+        className="cursor-pointer"
+      />
+      <div
+        className={cn(
+          'absolute top-6 right-0 p-4 hidden group-hover:block text-sm bg-black/80 text-white shadow-lg rounded-lg w-xs',
+          direction === 'right' && 'translate-x-full',
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
